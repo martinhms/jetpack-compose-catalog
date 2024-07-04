13f9c371866b6b460ec9504cff0d7dd4d75c4af6
Martin Horacio Mendez <martinhmendez@MacBook-Pro-de-Martin.local>
2024-07-04 11:48:32 -0300

    Added preview in radioButton

--- app/src/main/java/com/example/jetpackcomposetuto/RadioButton.kt
+++ app/src/main/java/com/example/jetpackcomposetuto/RadioButton.kt
@@ -3,16 +3,24 @@ package com.example.jetpackcomposetuto
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MyRadioButtonList(name: String, onItemSelected: (String) -> Unit) {
 
-    Column(Modifier.fillMaxWidth()) {
+    Column(Modifier.fillMaxWidth().clip(RoundedCornerShape(36.dp))) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             RadioButton(selected = name == "Ejemplo 1", onClick = { onItemSelected("Ejemplo 1") })
             Text(text = "Ejemplo 1")
@@ -30,4 +38,17 @@ fun MyRadioButtonList(name: String, onItemSelected: (String) -> Unit) {
             Text(text = "Ejemplo 4")
         }
     }
+}
+
+@Preview(showBackground = true,
+    //device = Devices.PHONE,
+    //showSystemUi = true
+    )
+@Composable
+fun RadioButtonPreview() {
+    var selected by remember {
+        mutableStateOf("")
+    }
+    MyRadioButtonList(selected) { selected = it }
+
 }
